bfee50e1c838b9463716e00c19a0f09976391153
Santino <mellamoalroberto@gmail.com>
2024-06-05 14:03:39 -0300

    tokenValidate

--- controllers/mercadopago.js
+++ controllers/mercadopago.js
@@ -11,8 +11,8 @@ const REDIRECT_URI = process.env.MERCADOPAGO_REDIRECT_URI;
 
 const cuentaMercadoPago = async (req, res) => {
     try {
-        const { token } = req.cookies;
-        console.log(token)
+        const userId = req.user ? req.user.id : null;
+        console.log(userId)
 
         if (!code) {
             const authURL = `https://auth.mercadopago.com.ar/authorization?client_id=${CLIENT_ID}&response_type=code&platform_id=mp&redirect_uri=${REDIRECT_URI}`;
