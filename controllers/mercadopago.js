64a838f642b7c9c52f126e61e5d876d4a73514e3
Santino <mellamoalroberto@gmail.com>
2024-06-10 13:31:15 -0300

    .

--- controllers/mercadopago.js
+++ controllers/mercadopago.js
@@ -10,17 +10,8 @@ const REDIRECT_URI = process.env.MERCADOPAGO_REDIRECT_URI;
 
 const cuentaMercadoPago = async (req, res) => {
     try {
-        const { code } = req.query
         const { access_token, user} = req.body
         console.log(access_token, user)
-
-        const response = await axios.post('https://api.mercadopago.com/oauth/token', {
-            grant_type: 'authorization_code',
-            client_id: CLIENT_ID,
-            client_secret: CLIENT_SECRET,
-            code: code,
-            redirect_uri: REDIRECT_URI
-        });
 
         const userFound = await User.findOne({ user });
         console.log(userFound)
@@ -33,7 +24,6 @@ const cuentaMercadoPago = async (req, res) => {
             return res.status(404).json({ message: 'Usuario no encontrado' });
         }
 
-        res.json({ message: 'Cuenta de MercadoPago enlazada exitosamente!', access_token });
     } catch (error) {
         console.error('Error al enlazar la cuenta de MercadoPago:', error);
         res.status(500).json({ message: error.message });
