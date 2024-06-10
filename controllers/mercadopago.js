e8481a00709918a32ac63e37ff8258807e166e12
Santino <mellamoalroberto@gmail.com>
2024-06-10 13:58:34 -0300

    .

--- controllers/mercadopago.js
+++ controllers/mercadopago.js
@@ -10,11 +10,25 @@ const REDIRECT_URI = process.env.MERCADOPAGO_REDIRECT_URI;
 
 const cuentaMercadoPago = async (req, res) => {
     try {
+        const { code } = req.query
         const { access_token, user} = req.body
         console.log(access_token, user)
 
+        if (!code) {
+            const authURL = `https://auth.mercadopago.com.ar/authorization?client_id=${CLIENT_ID}&response_type=code&platform_id=mp&state=${CLIENT_SECRET}&redirect_url=${REDIRECT_URI}`;
+            res.redirect(authURL);
+        }
+
+        const response = await axios.post('https://api.mercadopago.com/oauth/token', {
+            grant_type: 'authorization_code',
+            client_id: CLIENT_ID,
+            client_secret: CLIENT_SECRET,
+            code: code,
+            redirect_uri: REDIRECT_URI
+        });
+
+        console.log('first')
         const userFound = await User.findOne({ user });
-        console.log(userFound)
         if (userFound) {
             userFound.mercadopagoAccessToken = access_token;
             await userFound.save();
@@ -22,6 +36,7 @@ const cuentaMercadoPago = async (req, res) => {
             return res.status(404).json({ message: 'Usuario no encontrado' });
         }
 
+        res.json({ message: 'Cuenta de MercadoPago enlazada exitosamente!', access_token });
     } catch (error) {
         console.error('Error al enlazar la cuenta de MercadoPago:', error);
         res.status(500).json({ message: error.message });
