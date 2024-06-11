273542fd3c68249dd23522b263efd94efe342271
Santino <mellamoalroberto@gmail.com>
2024-06-11 14:02:27 -0300

    validateToken

--- routes/mp.route.js
+++ routes/mp.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import cuentaMercadoPago, { checkoutMercadoPago } from '../controllers/mercadopago.js'
+import validateToken from '../middlewares/validateToken.js'
 const router = express.Router()
 
 router.route('/callback')
-    .get(cuentaMercadoPago)
-    .post(cuentaMercadoPago);
+    .get(validateToken, cuentaMercadoPago)
+    .post(validateToken, cuentaMercadoPago);
 
 router.post('/checkout', checkoutMercadoPago)
 
