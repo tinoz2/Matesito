03962086647709cc812d66f5c5de6dd744e4183a
Santino <mellamoalroberto@gmail.com>
2024-06-11 14:54:58 -0300

    .

--- middlewares/validateToken.js
+++ middlewares/validateToken.js
@@ -4,6 +4,7 @@ dotenv.config()
 
 const validateToken = (req, res, next) => {
     const token = req.cookies.token
+    console.log(token)
     if (!token)
         return res.status(401).json({ message: 'Token no encontrado' })
 
--- routes/mp.route.js
+++ routes/mp.route.js
@@ -1,11 +1,9 @@
 import express from 'express'
 import cuentaMercadoPago, { checkoutMercadoPago } from '../controllers/mercadopago.js'
-import validateToken from '../middlewares/validateToken.js'
 const router = express.Router()
 
-router.route('/callback')
-    .get(validateToken, cuentaMercadoPago)
-    .post(validateToken, cuentaMercadoPago);
+router.get('/callback', cuentaMercadoPago);
+router.post('/callback', cuentaMercadoPago);
 
 router.post('/checkout', checkoutMercadoPago)
 
